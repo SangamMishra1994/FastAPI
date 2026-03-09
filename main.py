10330605baec9e5ab9d517e7edcdc0dfde3388ca
Sangam Mishra <50078441+SangamMishra1994@users.noreply.github.com>
2026-03-09 14:20:18 +0530

    Add endpoint to retrieve all details in JSON format

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.getAll("/all_detail")
+def getAllInfo():
+    return {"message": "All the data in the Json format"}
