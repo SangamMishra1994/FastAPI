a6eb448b97b0b2cc13a0a55bfde5119d4e47c461
Sangam Mishra <50078441+SangamMishra1994@users.noreply.github.com>
2026-03-09 23:26:24 +0530

    Implement CRUD operations for TODO management
    Added CRUD operations for managing TODOs.

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
+TODOS = [
+    {"title": "Task 1", "description": "My Task 1", "is_completed": False},
+    {"title": "Task 2", "description": "My Task 2", "is_completed": True},
+    {"title": "Task 3", "description": "My Task 3", "is_completed": True},
+    {"title": "Task 4", "description": "My Task 4", "is_completed": False},
+]
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 
-@app.getAll("/all_detail")
-def getAllInfo():
-    return {"message": "All the data in the Json format"}
+@app.getAll("/todos/all")
+def get_all_todo():
+    return TODOS
+
+
+@app.post("/todos/add")
+def create_todo(new_todo=Body()):
+    TODOS.append(new_todo)
+    return {"message":"Success"}
+
+
+@app.delete("/todos/{title}")
+def delete_todo_by_title(title):
+    for index in range(len(TODOS)):
+        if TODOS[index][title].lower() == title.lower():
+            TODOS.pop(index)
+            return {"message":"Delete Successfully"}
+
+
+@app.put("/todos/update")
+def update_todo(existing_todo=Body()):
+    TODOS[0] = existing_todo
+    return {"message": "Updated Successfully"}
+    
+         
